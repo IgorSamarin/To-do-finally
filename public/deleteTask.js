aff73c6fcc9b550b1d69a94ef1410379a931041c
Igor Samarin <samarigor.work@gmail.com>
2021-02-19 19:13:34 +0300

    add animate

--- public/deleteTask.js
+++ public/deleteTask.js
@@ -1,9 +1,12 @@
 async function DeleteTask(id) {
-    const response = await fetch(`/api/${id}`, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
-    });
-    if (response.ok === true) {
+  const response = await fetch(`/api/${id}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response.ok === true) {
+    
+    setTimeout(() => {
       GetItems();
-    }
+    }, 1000);
   }
+}
