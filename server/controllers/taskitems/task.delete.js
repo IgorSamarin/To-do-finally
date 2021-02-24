34a61c8b5508e358d98b4fd3766c6ed96776d933
Igor Samarin <samarigor.work@gmail.com>
2021-02-24 16:59:40 +0300

    fix back

--- server/controllers/taskitems/task.delete.js
+++ server/controllers/taskitems/task.delete.js
@@ -2,14 +2,19 @@ const TaskItem = require('../../models').TaskItem;
 module.exports = {
   destroy: async (req, res) => {
     try {
-      const { id,} = req.params;
+      const { id } = req.params;
       const task = await TaskItem.findByPk(id);
       if (!task) {
         return res.status(404).send({
           message: 'Item Not Found!',
         });
       }
-      await TaskItem.destroy({where: {id}});
+      const todoTask = await TaskItem.destroy({ where: { id } });
+      if (!todoTask) {
+        return res.status(404).send({
+          message: 'Item Not Found',
+        });
+      }
       res.status(204).send(task);
     } catch (err) {
       res.status(500).send(err.message);
