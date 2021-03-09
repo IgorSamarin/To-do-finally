c73be19e6c17f2a5684a05ef037ce7bb4fb57fb2
Igor Samarin <samarigor.work@gmail.com>
2021-03-09 18:19:39 +0300

    Delete files

--- server/controllers/users/user.get.authenticate.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const authenticate = require('../../middleware/authenticate');
-const express = require('express');
-const router = express.Router();
-
-router.get('/user/auth', authenticate, async (req, res) => {
-});
-
-module.exports = router;
